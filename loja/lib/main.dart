20e74e39176b912438494a70b3e364b9d16377dc
Gabriela <gabriela.oliveira@ccc.ufcg.edu.br>
2019-09-19 17:43:32 -0300

    login com google

--- loja/lib/main.dart
+++ loja/lib/main.dart
@@ -1,7 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:loja/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
+
+final googleSignIn = GoogleSignIn();
+final auth = FirebaseAuth.instance;
+
+Future<Null> _ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSignIn.currentUser;
+  if (user == null) {
+    user = await googleSignIn.signInSilently();
+  }
+  if (user == null) {
+    user = await googleSignIn.signIn();
+  }
+  if (await auth.currentUser() == null) {
+    GoogleSignInAuthentication credentials =
+        await googleSignIn.currentUser.authentication;
+    await auth.signInWithCredential(
+      GoogleAuthProvider.getCredential(
+        idToken: credentials.idToken,
+        accessToken: credentials.accessToken,
+      ),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
