222f80148d5042ca4ecfcf643530017f34ba1373
Gabriela <gabriela.oliveira@ccc.ufcg.edu.br>
2019-10-31 09:56:59 -0300

    bloc e validators

--- gerente_loja/lib/screens/login_screen.dart
+++ gerente_loja/lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gerente_loja/blocs/login_bloc.dart';
 import 'package:gerente_loja/widgets/input_field.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -7,6 +8,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _loginBloc = LoginBloc();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,28 +34,39 @@ class _LoginScreenState extends State<LoginScreen> {
                     icon: Icons.person_outline,
                     obscure: false,
                     hint: "Usuário",
+                    stream: _loginBloc.outEmail,
+                    onChanged: _loginBloc.changeEmail,
                   ),
                   InputField(
                     icon: Icons.lock_outline,
                     obscure: true,
                     hint: "Senha",
+                    stream: _loginBloc.outPassword,
+                    onChanged: _loginBloc.changePassword,
                   ),
                   SizedBox(
                     height: 32,
                   ),
-                  SizedBox(
-                    height: 50,
-                    child: RaisedButton(
-                      color: Theme.of(context).primaryColor,
-                      child: Text(
-                        "Entrar",
-                        style: TextStyle(
-                          fontSize: 18,
+                  StreamBuilder<bool>(
+                    stream: _loginBloc.outSubmitValid,
+                    builder: (context, snapshot) {
+                      return SizedBox(
+                        height: 50,
+                        child: RaisedButton(
+                          color: Theme.of(context).primaryColor,
+                          disabledColor:
+                              Theme.of(context).primaryColor.withAlpha(140),
+                          child: Text(
+                            "Entrar",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          onPressed: snapshot.hasData ? () {} : null,
+                          textColor: Colors.white,
                         ),
-                      ),
-                      onPressed: () {},
-                      textColor: Colors.white,
-                    ),
+                      );
+                    },
                   ),
                 ],
               ),
