e8747f91ac37f93a0acd4864a913e1776e1cf625
Gabriela <gabriela.oliveira@ccc.ufcg.edu.br>
2019-10-31 14:44:05 -0300

    login

--- gerente_loja/lib/screens/login_screen.dart
+++ gerente_loja/lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gerente_loja/blocs/login_bloc.dart';
+import 'package:gerente_loja/screens/home_screen.dart';
 import 'package:gerente_loja/widgets/input_field.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,69 +12,117 @@ class _LoginScreenState extends State<LoginScreen> {
   final _loginBloc = LoginBloc();
 
   @override
+  void initState() {
+    super.initState();
+    _loginBloc.outState.listen((state) {
+      switch (state) {
+        case LoginState.SUCCESS:
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => HomeScreen()));
+          break;
+        case LoginState.FAIL:
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text("Erro"),
+              content: Text("Você não possui os privilégios necessários."),
+            ),
+          );
+          break;
+        case LoginState.IDLE:
+        case LoginState.LOADING:
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).primaryColor;
     return Scaffold(
       backgroundColor: Colors.grey[850],
-      body: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Container(),
-          SingleChildScrollView(
-            child: Container(
-              margin: EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: StreamBuilder<LoginState>(
+        stream: _loginBloc.outState,
+        builder: (context, snapshot) {
+          switch (snapshot.data) {
+            case LoginState.LOADING:
+              return Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(primaryColor),
+                ),
+              );
+            case LoginState.SUCCESS:
+            case LoginState.FAIL:
+            case LoginState.IDLE:
+              return Stack(
+                alignment: Alignment.center,
                 children: <Widget>[
-                  Image(
-                    image: AssetImage('assets/supergeeks-icon.png'),
-                    height: 100,
-                    width: 100,
-                  ),
-                  InputField(
-                    icon: Icons.person_outline,
-                    obscure: false,
-                    hint: "Usuário",
-                    stream: _loginBloc.outEmail,
-                    onChanged: _loginBloc.changeEmail,
-                  ),
-                  InputField(
-                    icon: Icons.lock_outline,
-                    obscure: true,
-                    hint: "Senha",
-                    stream: _loginBloc.outPassword,
-                    onChanged: _loginBloc.changePassword,
-                  ),
-                  SizedBox(
-                    height: 32,
-                  ),
-                  StreamBuilder<bool>(
-                    stream: _loginBloc.outSubmitValid,
-                    builder: (context, snapshot) {
-                      return SizedBox(
-                        height: 50,
-                        child: RaisedButton(
-                          color: Theme.of(context).primaryColor,
-                          disabledColor:
-                              Theme.of(context).primaryColor.withAlpha(140),
-                          child: Text(
-                            "Entrar",
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
+                  Container(),
+                  SingleChildScrollView(
+                    child: Container(
+                      margin: EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Image(
+                            image: AssetImage('assets/supergeeks-icon.png'),
+                            height: 100,
+                            width: 100,
                           ),
-                          onPressed: snapshot.hasData ? () {} : null,
-                          textColor: Colors.white,
-                        ),
-                      );
-                    },
+                          InputField(
+                            icon: Icons.person_outline,
+                            obscure: false,
+                            hint: "Usuário",
+                            stream: _loginBloc.outEmail,
+                            onChanged: _loginBloc.changeEmail,
+                          ),
+                          InputField(
+                            icon: Icons.lock_outline,
+                            obscure: true,
+                            hint: "Senha",
+                            stream: _loginBloc.outPassword,
+                            onChanged: _loginBloc.changePassword,
+                          ),
+                          SizedBox(
+                            height: 32,
+                          ),
+                          StreamBuilder<bool>(
+                            stream: _loginBloc.outSubmitValid,
+                            builder: (context, snapshot) {
+                              return SizedBox(
+                                height: 50,
+                                child: RaisedButton(
+                                  color: primaryColor,
+                                  disabledColor: primaryColor.withAlpha(140),
+                                  child: Text(
+                                    "Entrar",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  onPressed: snapshot.hasData
+                                      ? _loginBloc.submit
+                                      : null,
+                                  textColor: Colors.white,
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
-              ),
-            ),
-          ),
-        ],
+              );
+          }
+        },
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _loginBloc.dispose();
+    super.dispose();
   }
 }
